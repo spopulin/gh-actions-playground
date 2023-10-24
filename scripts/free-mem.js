eb0e0118462115b917e0264ff77ce775550f4f40
Stefano Populin <tefapopulin@gmail.com>
2023-10-24 00:03:53 -0300

    add clg

--- scripts/free-mem.js
+++ scripts/free-mem.js
@@ -1,4 +1,4 @@
-const os = require('os');
+const os = require("os");
 
 const totalMemoryInBytes = os.totalmem();
 const freeMemoryInBytes = os.freemem();
@@ -6,5 +6,6 @@ const freeMemoryInBytes = os.freemem();
 const totalMemoryInMB = (totalMemoryInBytes / (1024 * 1024)).toFixed(2);
 const freeMemoryInMB = (freeMemoryInBytes / (1024 * 1024)).toFixed(2);
 
-console.log('Total memory (in MB):', totalMemoryInMB);
-console.log('Free memory (in MB):', freeMemoryInMB);
+console.log("Total memory (in MB):", totalMemoryInMB);
+console.log("Free memory (in MB):", freeMemoryInMB);
+console.log("Push sth:");
